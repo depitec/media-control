6143f1a6a6d690fd54a8ffa1678edba407ee5f16
Jan Schmidt <hi@jnsmdt.dev>
2025-02-15 17:29:37 +0100

    feat: added basic register_pin function and added more state to Pin class

--- service/PinController/Pin.py
+++ service/PinController/Pin.py
@@ -7,20 +7,49 @@ if TYPE_CHECKING:
     from .PinController import PinController
 
 
-class PinState(enum.Enum):
+class PIN_STATE(enum.Enum):
     ACTIVE = 1
     INACTIVE = 0
 
 
-class PinType(enum.Enum):
+class PIN_TYPE(enum.Enum):
     INPUT = GPIO.IN
     OUTPUT = GPIO.OUT
 
 
+class PIN_TRIGGER_TYPE(enum.Enum):
+    IMPULSE = 1
+    HOLD = 2
+
+
 class Pin:
-    def __init__(self, controller: PinController, gpio_pin, type):
+    def __init__(
+        self,
+        controller: PinController,  # fixed value never change
+        gpio_pin: int,  # fixed value never change
+        pin_type: PIN_TYPE = PIN_TYPE.INPUT,  # fixed value never change
+        trigger_type: PIN_TRIGGER_TYPE = PIN_TRIGGER_TYPE.IMPULSE,
+        trigger_hold_time: int = 5,  # in seconds
+        is_blocked: bool = False,
+    ):
         self.controller = controller
         self.gpio_pin = gpio_pin
-        # setup pin
+        self.type = pin_type
+        self.is_blocked = is_blocked
+        self.trigger_type = trigger_type
+        self.trigger_hold_time = trigger_hold_time
 
-        self.state: PinState = PinState.INACTIVE
+    def change_trigger_type(self, trigger_type: PIN_TRIGGER_TYPE):
+        if trigger_type == self.trigger_type:
+            return
+        self.trigger_type = trigger_type
+
+    def change_trigger_hold_time(self, trigger_hold_time: int):
+        if trigger_hold_time == self.trigger_hold_time:
+            return
+        self.trigger_hold_time = trigger_hold_time
+
+    def change_blocked_state(self, is_blocked: bool):
+        if is_blocked == self.is_blocked:
+            return
+        self.is_blocked = is_blocked
--- service/PinController/PinController.py
+++ service/PinController/PinController.py
@@ -1,13 +1,18 @@
-from __future__ import annotations
 import RPi.GPIO as GPIO
-from typing import TYPE_CHECKING
+from .Pin import PIN_TYPE, Pin
 
-if TYPE_CHECKING:
-    from Pin import PinType
 
 class PinController:
     def __init__(self):
+        self.pins = {}
         GPIO.setmode(GPIO.BCM)
 
-    def register_pin(self, pin_number, pin_type: PinType):
-        pass
+    def register_pin(self, pin_number: int, pin_type: PIN_TYPE):
+        GPIO.setup(
+            pin_number, pin_type.value
+        )  # using the value of the enum to satisfy the type checker
+        self.pins[pin_number] = Pin(self, pin_number, pin_type)
+
+    def unregister_pin(self, pin_number: int):
+        GPIO.cleanup(pin_number)
+        del self.pins[pin_number]
