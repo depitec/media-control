e6921988808226d9daff969c6e919b903c8d01e0
Jan Schmidt <hi@jnsmdt.dev>
2025-03-10 14:14:58 +0100

    feat: added trigger delay for inputpin + added missing state change

--- service/PinManager/Pins/InputPin.py
+++ service/PinManager/Pins/InputPin.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from .Pin import Pin
 from typing import Union, TYPE_CHECKING
+from time import sleep
 from datetime import datetime
 
 
@@ -15,6 +16,7 @@ type TriggerablePins = Union[OutputPin, VirtualPin]
 
 class InputPin(Pin):
     trigger_pins: list[TriggerablePins]
+    trigger_delay: float  ## Time in seconds to wait before triggering the output pin
 
     def __init__(
         self,
@@ -23,6 +25,7 @@ class InputPin(Pin):
     ):
         super().__init__(name, gpio_pin, "input")
         self.trigger_pins = []
+        self.trigger_delay = 0
 
     def add_trigger_pin(self, pin: TriggerablePins):
         self.trigger_pins.append(pin)
@@ -31,8 +34,12 @@ class InputPin(Pin):
         self.trigger_pins.remove(pin)
 
     def activate(self, trigger_context: TriggerContext):
+        self.state = "active"
+
         activate_time = datetime.timestamp(datetime.now())
         context = (self, activate_time)
+
+        sleep(self.trigger_delay)
 
         for pin in self.trigger_pins:
             pin.trigger(context)
